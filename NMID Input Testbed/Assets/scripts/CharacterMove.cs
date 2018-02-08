3bb4853da8c86f1cd5c684118ddc6addf5045499
January Gorsline <theironskies@gmail.com>
2018-02-08 07:07:57 -0500

    Initial Commit - Added basic rotation and movement and a few drafts of input manager scripts for when we connect to Arduino

---
+++ NMID Input Testbed/Assets/scripts/CharacterMove.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Character.BasicControls
+{
+    [RequireComponent(typeof (Rigidbody))]
+    public class CharacterMove : MonoBehaviour
+    {
+        public class MoveSettings
+        {
+            public float forwardSpeed = 4.0f;
+            public float strafeSpeed = 2.5f;
+            public float backSpeed = 1.0f;
+            public float targetSpeed = 4.0f;
+
+            public KeyCode Left = KeyCode.LeftArrow;
+            public KeyCode Right = KeyCode.RightArrow;
+            public KeyCode Forward = KeyCode.UpArrow;
+
+            public void UpdateMovement()
+            {
+                //if (input == Vector2.zero) return;
+                //if (input.x > 0 || input.x < 0)
+                //{
+                //    targetSpeed = strafeSpeed;
+                //}
+                //if (input.y < 0)
+                //{
+                //
+                //}
+                if (Input.GetKey(Right))
+                {
+                    
+                }
+                if (Input.GetKey(Left))
+                {
+                    
+                }
+            }
+        }
+    }
+}
+
---
+++ NMID Input Testbed/Assets/scripts/CharacterRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterRotate : MonoBehaviour {
+
+    public float mouseSensitivity = 0.0f;
+    public Vector3 mouseDelta = Vector3.zero;
+    private Vector3 prevMouseDelta = Vector3.zero;
+
+    void Update()
+    {
+        mouseDelta = (Input.mousePosition - prevMouseDelta) * mouseSensitivity;
+        prevMouseDelta = Input.mousePosition;
+        float rotZ = mouseDelta.x * Mathf.Deg2Rad;
+        transform.Rotate(Vector3.forward, -rotZ);
+    }
+}
