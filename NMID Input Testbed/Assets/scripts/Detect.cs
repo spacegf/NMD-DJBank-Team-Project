573eb87a368eb804e99cc88400e0b4e757a54339
Donald Pelcher <dap4092@rit.edu>
2018-03-14 14:59:40 -0400

    Detect uses key input

--- NMID Input Testbed/Assets/scripts/Detect.cs
+++ NMID Input Testbed/Assets/scripts/Detect.cs
@@ -3,10 +3,22 @@ using System.Collections;
 
 public class Detect : MonoBehaviour {
     
-    void OnTriggerEnter(Collider other) {
-		     if(other.gameObject.tag == "Enemy")
-     {
-        Destroy(other.gameObject);
-	 }
+	public KeyCode k = KeyCode.K;
+	
+   // void OnTriggerEnter(Collider other) {
+		
+		void OnTriggerStay(Collider other)
+{
+	 if(other.gameObject.tag == "Enemy")
+	{
+		print("Still colliding with trigger object " + other.name);
+			
+			if (Input.GetKeyUp(k))
+			{Destroy(other.gameObject);}
+	}
+    
 }
+       
+		     
+
 }
