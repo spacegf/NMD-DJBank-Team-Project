e0ececb411a687cbfcded43bb2e6497d92b1df74
Donald Pelcher <dap4092@rit.edu>
2018-03-24 12:36:16 -0400

    Add files via upload

---
+++ SplineMaster/Assets/scripts/Shot.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Shot : MonoBehaviour {
+   
+ void OnTriggerEnter(Collider other)
+{
+	if(other.gameObject.tag == "Enemy"){	
+	Destroy(other.gameObject);
+		}
+    print("Collision detected with trigger object " + other.name);
+	//if(other.gameObject.tag != "spaw"){	
+	Destroy(gameObject);
+//}
+}
+}
