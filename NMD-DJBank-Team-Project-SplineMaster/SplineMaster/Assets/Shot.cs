2786d0c4142445893d23fc788584c888da2cd8c6
Donald <wayofboon@gmail.com>
2018-03-27 09:28:17 -0400

    update

---
+++ NMD-DJBank-Team-Project-SplineMaster/SplineMaster/Assets/Shot.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class Shot : MonoBehaviour {
+   
+ void OnTriggerEnter(Collider other)
+{
+	if(other.gameObject.tag == "Enemy"){	
+	Destroy(other.gameObject);
+		}
+    //print("Collision detected with trigger object " + other.name);
+	if(other.gameObject.tag != "Spaw"){	
+	if(other.gameObject.tag != "Blast"){	
+	print("Deleted " + other.name);
+	Destroy(gameObject);
+}
+}
+}
+}
