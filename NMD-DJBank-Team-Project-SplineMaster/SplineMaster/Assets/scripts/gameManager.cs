754575e9573527e64c112cc967f8bc332d4d6237
Donald <wayofboon@gmail.com>
2018-04-05 08:27:59 -0400

    integrating

---
+++ NMD-DJBank-Team-Project-SplineMaster/SplineMaster/Assets/scripts/gameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour {
+    Vector3 dir;
+    public float speed;
+
+    void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+        dir = transform.localPosition;
+        //dir.y = transform.position.y;
+        dir.Normalize();
+        //transform.Translate(dir * speed * Time.deltaTime);
+
+        
+    }
+}
